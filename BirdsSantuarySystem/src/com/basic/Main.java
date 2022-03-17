43ea7d383dc91aa96b2d811c4e1e0064caba8bf7
Chougulenik <99316813+Chougulenik@users.noreply.github.com>
2022-03-17 11:37:25 -0400

    Add files via upload
    new added

--- BirdsSantuarySystem/src/com/basic/Main.java
+++ BirdsSantuarySystem/src/com/basic/Main.java
@@ -5,37 +5,39 @@ public class Main {
 		
 		System.out.println("welcome to Bird Sanctuary System");
 		
-		Pigeon pigeon = new Pigeon("d001");
-		Ostrich ostrich = new Ostrich("d002");
-		Duck duck = new Duck("d003");
-		Penguin penguin = new Penguin("d004");
-		Penguin penguin2 = new Penguin("d004");
-		Parrot parrot = new Parrot("d005");
-		
-		BirdRepository birdRepository = new BirdRepository();
-		
-		birdRepository.add(duck);
-		birdRepository.add(ostrich);
-		birdRepository.add(parrot);
-		birdRepository.add(penguin);
-		birdRepository.add(penguin2);
-		birdRepository.add(pigeon);
-		
 		UserInterface user = new UserInterface();
-		user.print(birdRepository.getBirdList());
-		System.out.println("---------------------");
-		user.printFlyable(birdRepository.getBirdList());
-		System.out.println("---------------------");
-		user.printSwimable(birdRepository.getBirdList());
-		System.out.println("---------------------");
-		user.printEatable(birdRepository.getBirdList());
-		
-		birdRepository.remove(parrot);
-		birdRepository.remove(penguin);
-		System.out.println("After remove 2 birds");
-		
-		user.print(birdRepository.getBirdList());
+		int option = user.showMainMenu();
 		
 		
+//		Birds pigeon = new Pigeon("d001");
+//		Birds ostrich = new Ostrich("d002");
+//		Birds duck = new Duck("d003");
+//		Birds penguin = new Penguin("d004");
+//		Birds penguin2 = new Penguin("d004");
+//		Birds parrot = new Parrot("d005");
+//		
+//		BirdRepository birdRepository = new BirdRepository();
+//		
+//		birdRepository.add(duck);
+//		birdRepository.add(ostrich);
+//		birdRepository.add(parrot);
+//		birdRepository.add(penguin);
+//		birdRepository.add(penguin2);
+//		birdRepository.add(pigeon);
+//		
+//		UserInterface user = new UserInterface();
+//		user.print(birdRepository.getBirdList());
+//		System.out.println("---------------------");
+//		user.printFlyable(birdRepository.getBirdList());
+//		System.out.println("---------------------");
+//		user.printSwimable(birdRepository.getBirdList());
+//		System.out.println("---------------------");
+//		user.printEatable(birdRepository.getBirdList());
+//		
+//		birdRepository.remove(parrot);
+//		birdRepository.remove(penguin);
+//		System.out.println("After remove 2 birds");
+//		
+//		user.print(birdRepository.getBirdList());	
 	}
 }
--- BirdsSantuarySystem/src/com/basic/UserInterface.java
+++ BirdsSantuarySystem/src/com/basic/UserInterface.java
@@ -1,18 +1,19 @@
 package com.basic;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class UserInterface {
 	
 	void print(Set birdList) {
-		for (Object bird : birdList) {
+		for (Object bird: birdList) {
 			System.out.println(bird);
 		}
 	}
 
 	void printFlyable(Set<Birds> birdList) {
-		for (Birds bird : birdList) {
+		for (Birds bird: birdList) {
 			if(bird instanceof Flyable) {
 				((Flyable)bird).fly();
 			}
@@ -20,7 +21,7 @@ public class UserInterface {
 	}
 
 	void printSwimable(Set<Birds> birdList) {
-		for (Birds bird : birdList) {
+		for (Birds bird: birdList) {
 			if(bird instanceof Swimmable) {
 //				((Swimmable)bird).swim();
 				Swimmable swimmableBird = (Swimmable)bird;
@@ -30,8 +31,16 @@ public class UserInterface {
 	}
 
 	void printEatable(Set<Birds> birdList) {
-		for (Birds bird : birdList) {
+		for (Birds bird: birdList) {
 			bird.eat();
 		}
+	}
+	
+	int showMainMenu() {
+		System.out.println("Enter 1: Add \n2: Remove \n3: Edit \n4: Printing \n5: Exit");
+		Scanner input = new Scanner(System.in);
+		int option = input.nextInt();
+		return option;
+		
 	}
 }
